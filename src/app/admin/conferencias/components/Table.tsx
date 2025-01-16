c3740adcb4b67697134dc8e96cfdf305fdc5f1c8
samuenoc <sreyesz@unah.hn>
2025-01-16 13:38:13 -0600

    ver conferencias

--- src/app/admin/conferencias/components/Table.tsx
+++ src/app/admin/conferencias/components/Table.tsx
@@ -1,53 +1,47 @@
-'use client';
+'use client'
 
-import { useState, useEffect} from "react";
-import { FaEdit, FaSearch, FaEye, FaPaperPlane } from "react-icons/fa";
-import { obtenerUsuarios } from "@/services/participantes/participantes";
-import { Participantes } from "@/interfaces/participantes";
+import { useState, useEffect } from "react";
+import { FaEdit, FaSearch, FaEye, FaFolderMinus } from "react-icons/fa";
+import { eliminarConferencia, obtenerConferencias } from "@/services/conferencias/conferencia";
+import { Conferencia } from "@/interfaces/conferencias";
 import { useRouter } from "next/navigation";
 import Loader from "@/components/Loading";
-
-// Componente de alerta reutilizable
-const Alert = ({ message, type }: { message: string; type: "success" | "error" }) => {
-  const bgColor = type === "success" ? "bg-green-500" : "bg-red-500";
-  return (
-    <div className={`${bgColor} text-white px-4 py-2 rounded shadow-lg fixed top-4 right-4`}>
-      {message}
-    </div>
-  );
-};
+import Modal from "@/components/Modal";
 
 const TableComponent = () => {
-  const [usuarios, setUsuarios] = useState<Participantes[]>([]);
   const router = useRouter();
-  const [filteredData, setFilteredData] = useState<Participantes[]>([]);
+  const [conferencias, setConferencias] = useState<Conferencia[]>([]);
+  const [filteredData, setFilteredData] = useState<Conferencia[]>([]);
   const [sortOrder, setSortOrder] = useState<"ASC" | "DESC">("ASC");
-  const [alert, setAlert] = useState<{ message: string; type: "success" | "error" } | null>(null);
+  const [, setActions] = useState<string[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [deleteShowModal, setDeleteShowModal] = useState<boolean>(false);
 
+  const fetchConferencias = async () => {
+    try {
+      setLoading(true);
+      const data = await obtenerConferencias(null);
+      setConferencias(data);
+      setFilteredData(data);
+      setActions(data.map(() => "Enviar"));
+    } catch (error) {
+      console.error(error);
+      setError("Error al cargar los conferencias.");
+    } finally {
+      setLoading(false);
+    }
+  };
+  
   useEffect(() => {
-    const fetchUsuarios = async () => {
-      try {
-        setLoading(true);
-        const data = await obtenerUsuarios();
-        setUsuarios(data);
-        setFilteredData(data);
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      } catch (err) {
-        setError("Error al cargar los usuarios.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchUsuarios();
+    fetchConferencias();
   }, []);
+  
 
   const toggleSortOrder = () => {
     const sortedData = [...filteredData].sort((a, b) => {
-      const comparison = a.nombre_completo.localeCompare(b.nombre_completo);
+      const comparison = a.nombre_ponente.localeCompare(b.nombre_ponente);
       return sortOrder === "ASC" ? -comparison : comparison;
     });
 
@@ -57,36 +51,46 @@ const TableComponent = () => {
 
   const handleSearch = (term: string) => {
     setSearchTerm(term);
-    const filtered = usuarios.filter(
-      (usuario) =>
-        usuario.nombre_completo.toLowerCase().includes(term.toLowerCase()) ||
-        usuario.correo.toLowerCase().includes(term.toLowerCase()) ||
-        usuario.dni.includes(term)
+    const filtered = conferencias.filter(
+      (conferencia) =>
+        conferencia.titulo.toLowerCase().includes(term.toLowerCase()) ||
+        conferencia.nombre_ponente.toLowerCase().includes(term.toLowerCase()) ||
+        conferencia.datosimportantes.join(", ").toLowerCase().includes(term.toLowerCase())
     );
     setFilteredData(filtered);
   };
 
-  const enviarCertificado = async (id: string) => {
-    try {
-      const response = await fetch(`https://backend-congreso.vercel.app/admin/certificates/send/${id}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
 
-      if (response.ok) {
-        setAlert({ message: `Certificado enviado exitosamente`, type: "success" });
-      } else {
-        const errorData = await response.json();
-        setAlert({ message: `Error al enviar el certificado: ${errorData.message}`, type: "error" });
-      }
-    } catch (error) {
-      setAlert({ message: `Error al enviar el certificado: ${error}`, type: "error" });
-    }
-
-    setTimeout(() => setAlert(null), 3000);
+  const handleView = (index: number) => {
+    router.push(`/admin/conferencias/${index}?visualizar=true`);
   };
+
+  const handleEdit = (index: number) => {
+    router.push(`/admin/conferencias/${index}?visualizar=false`);
+  };
+
+  const handleDelete = async (id: number) => {
+    try {
+      const result = await eliminarConferencia(id);
+      console.info("Conferencia eliminada:", result);
+      await fetchConferencias();
+    } catch (error) {
+      console.error("No se pudo eliminar la conferencia:", error);
+    }
+  };
+
+  const handleModal = async () =>{
+    setDeleteShowModal(true)
+  }
+
+
+  // const handleActionChange = (index: number) => {
+  //   setActions((prev) => {
+  //     const updatedActions = [...prev];
+  //     updatedActions[index] = updatedActions[index] === "Enviar" ? "Editar" : "Enviar";
+  //     return updatedActions;
+  //   });
+  // };
 
   if (loading) {
     return (
@@ -101,9 +105,9 @@ const TableComponent = () => {
   }
 
   return (
-    <div className="overflow-x-auto px-4">
-      {alert && <Alert message={alert.message} type={alert.type} />}
-      <h2 className="text-3xl mb-6 text-black border-b-[1px] border-gray-300 pb-1">Participantes</h2>
+    <div className="overflow-x-auto">
+      <h2 className="text-3xl mb-6 text-black border-b-[1px] border-gray-300 pb-1">Conferencias</h2>
+
       <div className="flex flex-wrap justify-between items-center mb-4 gap-2">
         <div className="flex items-center w-full sm:w-1/3 bg-gray-100 rounded-full px-4 py-1">
           <input
@@ -113,7 +117,7 @@ const TableComponent = () => {
             onChange={(e) => handleSearch(e.target.value)}
             className="w-full bg-transparent border-none text-gray-600 focus:outline-none"
           />
-          <button title="buscar" className="ml-2">
+          <button title="Buscar" className="ml-2">
             <FaSearch className="text-blue-500" />
           </button>
         </div>
@@ -131,43 +135,61 @@ const TableComponent = () => {
         <table className="min-w-full bg-white">
           <thead>
             <tr className="bg-gray-100">
-              <th className="px-4 py-2 text-left border-b">DNI</th>
-              <th className="px-4 py-2 text-left border-b">Nombre</th>
-              <th className="px-4 py-2 text-left border-b">Correo</th>
-              <th className="px-4 py-2 text-center border-b">Acciones</th>
+              <th className="px-4 py-2 text-left border-b">Título</th>
+              <th className="px-4 py-2 text-left border-b">Ponente</th>
+              <th className="px-4 py-2 text-left border-b">Lugar</th>
+              <th className="px-4 py-2 text-left border-b">Horario</th>
+              <th className="px-4 py-2 text-left border-b">Fecha</th>
+              <th className="px-4 py-2 text-left border-b">Cupos</th>
+              <th className="px-4 py-2 text-left border-b">Acciones</th>
             </tr>
           </thead>
-          <tbody className="font-thin">
-            {filteredData.map((usuario, index) => (
+          <tbody>
+            {filteredData.map((conferencia, index) => (
+              
               <tr
-                key={usuario.id_usuario}
+                key={index}
                 className={index % 2 === 0 ? "bg-gray-50" : "bg-white"}
               >
-                <td className="px-4 py-2 border-b">{usuario.dni}</td>
-                <td className="px-4 py-2 border-b">{usuario.nombre_completo}</td>
-                <td className="px-4 py-2 border-b">{usuario.correo}</td>
+                <td className="px-4 py-2 border-b">{conferencia.titulo}</td>
+                <td className="px-4 py-2 border-b">{conferencia.nombre_ponente}</td>
+                <td className="px-4 py-2 border-b">{conferencia.lugar}</td>
+                <td className="px-4 py-2 border-b">{conferencia.horario}</td>
+                <td className="px-4 py-2 border-b">{conferencia.fecha}</td>
+                <td className="px-4 py-2 border-b">{conferencia.cupos_disponibles}</td>
                 <td className="px-4 py-2 border-b text-center">
                   <div className="flex flex-col lg:flex-row items-center justify-center gap-1">
+                    {deleteShowModal && (
+                      <Modal
+                        message="¿Deseas eliminar esta conferencia?"
+                        subMessage={conferencia.titulo}
+                        onClose={() => setDeleteShowModal(false)}
+                        onSuccess={() => {
+                          setDeleteShowModal(false);
+                          handleDelete(conferencia.id_conferencia)
+                        }}
+                      />
+                    )}
                     <button
-                      className="bg-blue-500 text-white px-2 py-1 rounded shadow hover:bg-blue-600 text-sm font-300 flex items-center gap-1 w-full"
-                      onClick={() => enviarCertificado(usuario.id_usuario.toString())}
+                      onClick={() => handleView(conferencia.id_conferencia)}
+                      className={`bg-blue-500 text-white px-2 py-1 rounded shadow hover:bg-blue-600 text-sm font-300 flex items-center gap-1 w-full`}
                     >
-                      <FaPaperPlane size={13} />
-                      Enviar
+                      <FaEye size={20} />
+                      Ver
                     </button>
                     <button
-                      className="bg-yellow-500 text-white px-2 py-1 rounded shadow hover:bg-yellow-600 text-sm font-300 flex items-center gap-1 w-full"
-                      onClick={() => router.push(`/admin/home/${usuario.id_usuario}?visualizar=false`)}
+                      onClick={() => handleEdit(conferencia.id_conferencia)}
+                      className={`bg-[#f8b133] text-white px-2 py-1 rounded shadow hover:bg-blue-600 text-sm font-300 flex items-center gap-1 w-full`}
                     >
-                      <FaEdit size={15} />
+                      <FaEdit size={20} />
                       Editar
                     </button>
                     <button
-                      className="bg-green-500 text-white px-2 py-1 rounded shadow hover:bg-green-600 text-sm font-300 flex items-center gap-1 w-full"
-                      onClick={() => router.push(`/admin/home/${usuario.id_usuario}?visualizar=true`)}
+                      onClick={() => handleModal()}
+                      className={`bg-red-400 text-white px-2 py-1 rounded shadow hover:bg-blue-600 text-sm font-300 flex items-center gap-1 w-full`}
                     >
-                      <FaEye size={15} />
-                      Ver
+                      <FaFolderMinus size={20} />
+                      Ocultar
                     </button>
                   </div>
                 </td>
@@ -176,6 +198,8 @@ const TableComponent = () => {
           </tbody>
         </table>
       </div>
+
+
     </div>
   );
 };
